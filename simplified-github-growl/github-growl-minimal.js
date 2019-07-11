403a1aaeeabc3229946c23ea5917657a9d4c194c
fmkang <33063503+fmkang@users.noreply.github.com>
2019-07-11 17:41:04 -0600

    Update simplified growl

--- simplified-github-growl/github-growl-minimal.js
+++ simplified-github-growl/github-growl-minimal.js
@@ -1,11 +1,11 @@
 var growl = require('./growl'),
     GitHubApi = require('github');
 
-module.exports.github = new GitHubApi({
+var github = new GitHubApi({
     version: '3.0.0'
 });
 
-module.exports.growl = function(title, description) {
+var myGrowl = function(title, description) {
     growl(description, {
         title: title
     }, function(err) {
@@ -13,46 +13,17 @@ module.exports.growl = function(title, description) {
     });
 };
 
-module.exports.parseEvent = function(event) {
+var parseEvent = function(event) {
     var that = this;
     console.log('DEBUG', event.type);
-    switch (event.type) {
-        case 'CommitCommentEvent':
-            break;
-        case 'CreateEvent':
-            break;
-        case 'DeleteEvent':
-            break;
-        case 'DownloadEvent':
-            break;
-        case 'GollumEvent':
-            break;
-        case 'IssueCommentEvent':
-            title = title = 'Issue #' + event.payload.issue.number;
-            title = title + ' - ' + event.payload.issue.title;
-            description = description + 'New comment by @' + event.actor.login;
-            description = description + '\n';
-            description = description + event.payload.issue.html_url;
-            description = description + '#issuecomment-';
-            description = description + event.payload.comment.id;
-            break;
-        case 'IssuesEvent':
-            break;
-        case 'MemberEvent':
-            break;
-        case 'PullRequestEvent':
-            break;
-        case 'PullRequestReviewCommitEvent':
-            break;
-        case 'PushEvent':
-            break;
-        case 'TeamAddEvent':
-            break;
-        default:
-            title = title + 'Github Notification';
-            description = title + 'An event occured on Github, go check it out!';
-    }
-    that.growl(gravatar, title, description);
+    title = title = 'Issue #' + event.payload.issue.number;
+    title = title + ' - ' + event.payload.issue.title;
+    description = description + 'New comment by @' + event.actor.login;
+    description = description + '\n';
+    description = description + event.payload.issue.html_url;
+    description = description + '#issuecomment-';
+    description = description + event.payload.comment.id;
+    myGrowl(gravatar, title, description);
 };
 
 var monitorEvents = function(type, options) {
@@ -60,11 +31,15 @@ var monitorEvents = function(type, options) {
     that.github.events.getFromUser({
         user: options.username
     }, function(err, events) {
-        var event = events[0]
-        that.parseEvent(event);
+        that.events = events;
     });
+    var event = events[0];
+    parseEvent(event);
 };
 
+module.exports.github = github;
+module.exports.growl = myGrowl;
+module.exports.parseEvent = parseEvent;
 module.exports.monitorEvents = monitorEvents;
 
 monitorEvents({
--- simplified-github-growl/growl.js
+++ simplified-github-growl/growl.js
@@ -3,15 +3,20 @@ exports = module.exports = growl;
 
 function growl(msg, options, fn) {
     args = 'terminal-notifier';
-    args = args + '-message';
-    args = args + '"' + msg + '"';
+    args = args + ' -message';
+    args = args + ' "' + msg + '"';
     if (options.title) {
-        args = args + '-title';
-        args = args + '"' + options.title + '"';
+        args = args + ' -title';
+        args = args + ' "' + options.title + '"';
     }
     if (options.subtitle) {
-        args = args + '-subtitle';
-        args = args + '"' + options.subtitle + '"';
+        args = args + ' -subtitle';
+        args = args + ' "' + options.subtitle + '"';
     }
     exec(args, fn);
 };
+
+var msg = 'Hello';
+var options = {'title': 'Hello world'};
+
+growl(msg, options, function(){});
