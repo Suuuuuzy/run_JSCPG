9bc5e81f06d686ba21e32628e0a4488f8885f991
Song-Li <lisong1013@gmail.com>
2019-07-11 20:32:42 -0400

    small fix for grow

--- simplified-github-growl/github-growl-minimal.js
+++ simplified-github-growl/github-growl-minimal.js
@@ -16,7 +16,7 @@ var myGrowl = function(title, description) {
 var parseEvent = function(event) {
     var that = this;
     console.log('DEBUG', event.type);
-    title = title = 'Issue #' + event.payload.issue.number;
+    title = 'Issue #' + event.payload.issue.number;
     title = title + ' - ' + event.payload.issue.title;
     description = description + 'New comment by @' + event.actor.login;
     description = description + '\n';
@@ -26,7 +26,7 @@ var parseEvent = function(event) {
     myGrowl(gravatar, title, description);
 };
 
-var monitorEvents = function(type, options) {
+var monitorEvents = function(options) {
     var that = this;
     that.github.events.getFromUser({
         user: options.username
