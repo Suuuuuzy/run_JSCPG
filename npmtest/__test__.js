2ca9bf1f6f7ab33688e59c63a9a41920420f5d46
Song-Li <lisong1013@gmail.com>
2019-09-02 17:53:05 -0400

    a version of runable analyse

---
+++ npmtest/__test__.js
@@ -0,0 +1,2 @@
+var main_func=require('/media/data/lsong18/data/npmpackages/react-native-simple-download-manager/index.js');
+main_func('var');
--- npmtest/callfunctionGenerator.py
+++ npmtest/callfunctionGenerator.py
@@ -1,5 +1,8 @@
 import os
 import json 
+import sys
+sys.path.append("..")
+from simurun.launcher import *
 
 def get_list_of_packages(path, limit=None):
     """
@@ -47,11 +50,24 @@ def get_main_file_of_package(package_path):
     else:
         return None
 
+
 root_path = "/media/data/lsong18/data/npmpackages/"
 packages = get_list_of_packages(root_path, limit = 10)
 for package in packages:
+    print("Running {}".format(package))
     package_main_file = get_main_file_of_package("{}{}".format(root_path, package))
     if package_main_file is None:
-        print(package)
+        print("{} not found".format(package))
         continue
-    print(package_main_file)
+    js_call_templete = "var main_func=require('{}');\nmain_func('var');".format(package_main_file)
+    with open("__test__.js", 'w') as jcp:
+        jcp.write(js_call_templete)
+
+    G = unittest_main('__test__.js')
+    res_path = G.traceback("os-command")
+    line_path = res_path[0]
+    detailed_path = res_path[1]
+    if len(line_path) != 0:
+        print("Found path from {}: {}".format(package, line_path))
+
+
