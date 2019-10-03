128e6cde8bfe2bb3b8dc4ba65924fff60afff181
Song-Li <lisong1013@gmail.com>
2019-10-03 19:27:51 -0400

    update backtrace method to support multiple signature function call

--- simurun/vulFuncLists.py
+++ simurun/vulFuncLists.py
@@ -1,8 +1,22 @@
-os_command_list = [
-        "sink_execFile_hqbpillvul",
-        'sink_exec_hqbpillvul'
-        ]
-xss_list = [
-                'createServer',
-                'write',
-                'send'
+signature_lists = {
+        'os_command': [
+            "sink_execFile_hqbpillvul",
+            'sink_exec_hqbpillvul'
+            ],
+        'xss': [
+            'createServer',
+            'write',
+            'send'
+            ]
+}
+
+def get_all_sign_list():
+    """
+    return a list of all the signature functions
+    """
+    res = []
+    for key in signature_lists:
+        res += signature_lists[key]
+
+    return res
+
