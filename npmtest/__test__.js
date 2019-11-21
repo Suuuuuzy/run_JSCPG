a74352f4efe43c04bd6c9cfa7594bc03d2308f2a
Song-Li <lisong1013@gmail.com>
2019-11-21 16:25:58 -0500

    updated checking once finished require

--- npmtest/__test__.js
+++ npmtest/__test__.js
@@ -1,1 +1,1 @@
-var main_func=require('/media/data/lsong18/data/vulPackages/command_injection/printer@0.0.1/examples/example_xps_windows.js');
+var main_func=require('/media/data/lsong18/data/vulPackages/command_injection/opencv@6.0.0/examples/addweighted.js');
