6a906715f002e780f889f0ce956e25043dc6347c
Song-Li <lisong1013@gmail.com>
2020-02-10 15:22:46 -0500

    update a small bug

--- npmtest/chunkrun.sh
+++ npmtest/chunkrun.sh
@@ -1,6 +1,6 @@
 NUM_Thread=30
 for V in $(seq 1 $NUM_Thread);
 do 
-  screen -S runscreen_$V -dm python callfunctionGenerator.py -s -c $V $NUM_Thread -t path_traversal
+  screen -S runscreen_$V -dm python callfunctionGenerator.py -c $V $NUM_Thread -t path_traversal
 done
 
--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 //process.env.NM_CLI = 'touch sdfliajdlifja & nmcli';
 //var attack_str = ";touch Song;";
 //var root = require("./deadloop.js");
-var root = require("/media/data2/lsong18/data/pre_npmpackages/freemodjs/");
-//var root = require("/home/lsong18/projs/JSCPG/package_downloader/packages/set-value@3.0.0/index.js")
+//var root = require("/media/data2/lsong18/data/pre_npmpackages/freemodjs/");
+var root = require("/home/lsong18/projs/JSCPG/package_downloader/packages/general-file-server@1.1.8")
 //var root = require("/home/lsong18/projs/JSCPG/test/node_modules/undefsafe/lib/undefsafe.js")
 //root.getDevices();
 //v2vm: root.getDelpoyVer('"& touch sdfliajdlifja &"',123);
