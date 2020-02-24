b1d6067e548c04d54158f838383c956388bd0e5c
Song-Li <lisong1013@gmail.com>
2020-02-24 16:21:46 -0500

    reach 70

--- npmtest/chunkrun.sh
+++ npmtest/chunkrun.sh
@@ -1,8 +1,8 @@
 NUM_Thread=32
 for V in $(seq 1 $NUM_Thread);
 do 
-  screen -S runscreen_$V -dm python callfunctionGenerator.py -c $V $NUM_Thread -t path_traversal -s -l 120
-  #screen -S runscreen_$V -dm python callfunctionGenerator.py -c $V $NUM_Thread -t os_command -s -l 120
+  screen -S runscreen_$V -dm python callfunctionGenerator.py -c $V $NUM_Thread -t path_traversal
+  #screen -S runscreen_$V -dm python callfunctionGenerator.py -c $V $NUM_Thread -t os_command -l 120
   #screen -S runscreen_$V -dm python callfunctionGenerator.py -c $V $NUM_Thread -t code_exec -s
 done
 
