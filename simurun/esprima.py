8baa3648408fd3533f088736d465a4d5dd62dcaf
fmkang <33063503+fmkang@users.noreply.github.com>
2020-03-04 20:44:05 -0500

    Add esprima get_file_list

--- simurun/esprima.py
+++ simurun/esprima.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import re
 
 main_js_path = os.path.realpath(os.path.join(__file__,
                                 '../../esprima-joern/main.js'))
@@ -22,3 +23,19 @@ def esprima_search(module_name, search_path, print_func=print):
     print_func(stderr)
     main_path, module_path = stdout.split('\n')[:2]
     return main_path, module_path
+
+def get_file_list(module_name):
+    script = "var main_func=require('{}');".format(module_name)
+    # use "universal_newlines" instead of "text" if you're using Python <3.7
+    #        ↓ ignore this error if your editor shows
+    proc = subprocess.Popen(['node', main_js_path, '-', '-o', '-'], text=True,
+        stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = proc.communicate(script)
+    file_list = []
+    for match in re.finditer(
+        r'^[\u001b\[\w]*Analyzing ([^\u001b]*)[\u001b\[\w]*$',
+        stderr, flags=re.MULTILINE):
+            file_path = match.group(1)
+            if file_path != 'stdin':
+                file_list.append(file_path)
+    return file_list
