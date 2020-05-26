6b7fa8b0a274048c477db4eb89a1a5d7f4acad95
Song-Li <lisong1013@gmail.com>
2020-05-26 00:33:11 -0400

    update extract info

--- depd_analysis/extract_info.py
+++ depd_analysis/extract_info.py
@@ -1,5 +1,6 @@
 import json
 import os
+import argparse
 
 class DepdData:
 
@@ -17,9 +18,22 @@ class DepdData:
         return res_list
         return [os.path.basename(p) for p in res_list]
 
-depd = DepdData('./back_depd.json')
-undefsafe_use_list = depd.get_value('undefsafe')
-express_use_list = depd.get_value('express')
-both_use_list = [p for p in undefsafe_use_list if p in express_use_list]
-print(both_use_list)
+def main():
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('-f', nargs='+')
+    args = argparser.parse_args()
 
+    files = args.f
+    print(files)
+
+    depd = DepdData('./back_depd.json')
+
+    cur_list = []
+    for f in files:
+        if len(cur_list) == 0:
+            cur_list = depd.get_value(f)
+        else:
+            cur_list = [p for p in cur_list if p in depd.get_value(f)]
+    print(cur_list)
+
+main()
