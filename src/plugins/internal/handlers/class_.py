be91beacd81b429993e5719723dc8ec1ce6ab9b1
Song-Li <lisong1013@gmail.com>
2021-03-18 22:19:39 +0000

    still have class problems

--- src/plugins/internal/handlers/class_.py
+++ src/plugins/internal/handlers/class_.py
@@ -2,6 +2,7 @@ from src.plugins.handler import Handler
 from src.core.utils import ExtraInfo
 from src.core.graph import Graph
 from src.core.logger import loggers
+from . import func_decl 
 
 class HandleClass(Handler):
     """
@@ -17,16 +18,14 @@ class HandleMethod(Handler):
     def process(self):
         handle_method(self.G, self.node_id, self.extra)
 
+
 def handle_class(G: Graph, ast_node, extra):
     children = G.get_ordered_ast_child_nodes(ast_node)
     name = G.get_node_attr(children[0]).get('code')
     class_obj = G.add_obj_node(ast_node=None, js_type='function')
     G.set_node_attr(class_obj, ('name', name))
     G.set_node_attr(class_obj, ('value', f'[class {name}]'))
-    print(G.get_node_attr(children[4]))
     toplevel = children[4]
-    edges = G.get_out_edges(toplevel, edge_type='PARENT_OF')
-    print(edges)
     body = G.get_child_nodes(toplevel, edge_type='PARENT_OF',
                              child_type='AST_STMT_LIST')[0]
     prev_dont_quit = G.dont_quit
@@ -38,6 +37,18 @@ def handle_class(G: Graph, ast_node, extra):
         G.add_edge(class_obj, ast, {'type:TYPE': 'OBJ_TO_AST'})
     if G.find_nearest_upper_CPG_node(ast_node) == ast_node:
         G.add_obj_to_scope(name, tobe_added_obj=class_obj)
+    
+def handle_method(G: Graph, ast_node, extra):
+    assert extra.class_obj is not None
+    name = G.get_name_from_child(ast_node)
+    if name == 'constructor':
+        G.add_edge(extra.class_obj, ast_node, {'type:TYPE': 'OBJ_TO_AST'})
+    else:
+        method_obj = func_decl.decl_function(G, ast_node, add_to_scope=False)
+        prototypes = G.get_prop_obj_nodes(extra.class_obj, 'prototype', 
+                                          branches=extra.branches)
+        for p in prototypes:
+            G.add_obj_as_prop(name, parent_obj=p, tobe_added_obj=method_obj)
 
 def simurun_class_body(G, ast_node, extra):
     """
@@ -49,7 +60,7 @@ def simurun_class_body(G, ast_node, extra):
     else:
         branches = extra.branches
 
-    loggers.main_logger.info('BLOCK {} STARTS, Handling class, SCOPE {}'.format(ast_node, G.cur_scope))
+    loggers.main_logger.info('BLOCK {} STARTS, SCOPE {}'.format(ast_node, G.cur_scope))
     stmts = G.get_ordered_ast_child_nodes(ast_node)
     # control flows
     for last_stmt in G.last_stmts:
@@ -71,15 +82,3 @@ def simurun_class_body(G, ast_node, extra):
         if G.get_node_attr(stmt).get('type') == 'AST_RETURN':
             G.last_stmts = []
 
-def handle_method(G: Graph, ast_node, extra):
-    assert extra.class_obj is not None
-    from .func_decl import decl_function
-    name = G.get_name_from_child(ast_node)
-    if name == 'constructor':
-        G.add_edge(extra.class_obj, ast_node, {'type:TYPE': 'OBJ_TO_AST'})
-    else:
-        method_obj = decl_function(G, ast_node, add_to_scope=False)
-        prototypes = G.get_prop_obj_nodes(extra.class_obj, 'prototype', 
-                                          branches=extra.branches)
-        for p in prototypes:
-            G.add_obj_as_prop(name, parent_obj=p, tobe_added_obj=method_obj)
