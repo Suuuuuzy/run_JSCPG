44bcff0a970425f688603ae763719e608d583340
SuzyYu <zjyujianjia@163.com>
2021-07-13 23:00:05 +0800

    update run.py

--- run.py
+++ run.py
@@ -1,22 +1,16 @@
 import os
 # datapath = "/Volumes/Files/extension_research/data/unzipped_extensions"
-datapath = '/media/data2/song/extensions/unzipped_extensions'
+import sys
+datapath = sys.argv[1]
 # datapath = '/Users/jia/Desktop/tmp/EOPG/run_JSCPG/demos'
-files = os.listdir(datapath)
+with open(datapath) as f:
+    content = f.read()
+files = content.split('\n')
 # files = files[0:50]
 count = 0
 for file in files:
-    if os.path.isdir(os.path.join(datapath, file)):
-        with open('crx.log') as f:
-            content = f.read()
-        if file in content:
-  #          print(file, ' extension has been run')
-            continue
-   #     print('run', count, 'th extension')
+    if os.path.isdir(file):
         count+=1
         print('run extension ' + file + ' starts')
-        os.system('python3 generate_opg.py -t chrome_data_exfiltration -crx --timeout 60 ' + datapath + '/' + file)
-        # with open('crx.log', 'a') as f:
-        #     f.write('run extension: ' + file + '\n')
-        # 
+        os.system('./generate_opg.py -t chrome_data_exfiltration -crx --timeout 60 ' + file)
         
