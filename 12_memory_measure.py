b1cd2984fa609ce38c63297e6dc52dc80ae91e5d
Suuuuuzy <yujianjiasuzy@gmail.com>
2022-06-02 14:21:29 -0400

    measure memory and thread at the same time

--- 12_memory_measure.py
+++ 12_memory_measure.py
@@ -2,10 +2,10 @@ import os
 import json
 def main_func():
     path = "/media/data2/jianjia/extension_data/unzipped_extensions"
-    with open("memory_usage_measure/random_500.txt") as f:
+    with open("thread_measure/5_from_time.json") as f:
         ids = json.load(f)
     for id in ids:
-        cmd = "mprof run generate_opg.py -t chrome_ext -crx {extenion_path} -pq --timeout 600 -measure_thread"
+        cmd = "mprof run generate_opg.py -t chrome_ext -crx {extenion_path} -pq --timeout 600 -measure_thread -auto -no_merge"
         abs_id = os.path.join(path, id)
         os.system(cmd.format(extenion_path = abs_id))
 
