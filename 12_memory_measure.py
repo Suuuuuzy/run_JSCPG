a1f4885d0407d8d5d88d9441129eab433eb733a5
Suuuuuzy <yujianjiasuzy@gmail.com>
2022-06-03 03:42:05 -0400

    debug

--- 12_memory_measure.py
+++ 12_memory_measure.py
@@ -2,10 +2,11 @@ import os
 import json
 def main_func():
     path = "/media/data2/jianjia/extension_data/unzipped_extensions"
+    # path = "thread_measure/extensions"
     with open("thread_measure/5_from_time.json") as f:
         ids = json.load(f)
     for id in ids:
-        cmd = "mprof run generate_opg.py -t chrome_ext -crx {extenion_path} -pq --timeout 600 -measure_thread -auto -no_merge"
+        cmd = "mprof run generate_opg.py -t chrome_ext -crx {extenion_path} -pq --timeout 600 -measure_thread -auto -no_merge -measure_code_cov_progress"
         abs_id = os.path.join(path, id)
         os.system(cmd.format(extenion_path = abs_id))
 
