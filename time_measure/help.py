f7969db7ae29ab8db821257c648412aa118832a2
Suuuuuzy <yujianjiasuzy@gmail.com>
2022-06-07 14:46:53 -0400

    new 5

--- time_measure/help.py
+++ time_measure/help.py
@@ -1,14 +1,13 @@
 import os
 import json
 import random
-with open("time_id.txt") as f:
-	c = set(json.load(f))
-print(len(c))
+# with open("time_id.txt") as f:
+# 	c = set(json.load(f))
+# print(len(c))
 
-with open("random_500.txt") as f:
-	c_500 = set(json.load(f))
+with open("../code_cov_measure/500_timeout_ids.json") as f:
+	timeouts = (json.load(f))
 
-timeouts = list(c_500-c)
 select = []
 for i in range(5):
 	tmp = random.randint(0,len(timeouts)-1)
