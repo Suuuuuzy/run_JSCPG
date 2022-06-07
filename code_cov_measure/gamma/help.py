7f30e9e2c2e7694c73c1bab969024dd4ee004fb2
Suuuuuzy <yujianjiasuzy@gmail.com>
2022-06-07 11:48:42 -0400

    add data

--- code_cov_measure/gamma/help.py
+++ code_cov_measure/gamma/help.py
@@ -6,11 +6,18 @@ res = {}
 for i in files:
 	with open(i) as f:
 		res.update(json.load(f))
-
-for i in res:
-	all_cov = 0
-	for j in res[i]:
-		all_cov+=res[i][j]
-	ave = all_cov/len(res[i])
-	print(i, ave)
-	print(len(res[i]))
+with open("data.txt", "w") as f:
+	for i in res:
+		if i in ["0.5_0_1", "0_0_1", "policy_2", "policy_3", "policy_4"]:
+			continue
+		all_cov = 0
+		for j in res[i]:
+			all_cov+=res[i][j]
+		ave = all_cov/len(res[i])
+		if i=="policy_1":
+			name = "0_0_1"
+		else:
+			name = i
+		print(name, ave)
+		print(len(res[i]))
+		f.write(name + "\t" +  str(ave) + "\n")
