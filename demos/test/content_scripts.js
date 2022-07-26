17324a2d58510c6be23ec006eec99bfe928dd418
Suzy <yujianjiasuzy@gmail.com>
2022-07-26 16:46:45 -0400

    test demos

--- demos/test/content_scripts.js
+++ demos/test/content_scripts.js
@@ -1,12 +1,15 @@
-// window.addEventListener('message', event => {
-    var event;
-    if (event.data){
-        chrome.runtime.sendMessage('hfemecooncjiplidegaojnngpmelcgdd', event.data);
-        if (event.data=="anything" ){
-            var b = 4;
-        }
-    }
-    else{
-        console.log("else");
-    }
-// });
+// 1. Send the background a message requesting the user's data
+chrome.runtime.sendMessage('get-user-data', (response) => {
+  // 3. Got an asynchronous response with the data from the background
+  console.log('received user data', response);
+  // initializeUI(response);
+});
+
+
+
+chrome.runtime.sendMessage('get-user-data222', (response) => {
+  // 3. Got an asynchronous response with the data from the background
+  console.log('received 2', response);
+  console.log('received 3', response);
+  // initializeUI(response);
+});
